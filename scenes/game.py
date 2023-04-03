f911fcba87adfcda4ab63e20f93f4c5a273dfacd
Larissa Ember <lari.ember@hotmail.com>
2023-04-03 16:13:41 -0300

    come on

--- scenes/game.py
+++ scenes/game.py
@@ -1,3 +1,32 @@
-import pygame as pg
+import pygame
 
+pygame.init()
 
+#define the window size
+win_width = 800
+win_height = 800
+
+#create the window
+win = pygame.display.set_mode((win_width, win_height))
+
+#set the window title
+pygame.display.set_caption("my game")
+
+#vars for the game loop
+run = True
+clock = pygame.time.Clock()
+
+#loop
+while run:
+    #set the window refresh rate
+    clock.tick(60)
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+            
+    #window update
+    pygame.display.update()
+    
+#exit the game
+pygame.quit()

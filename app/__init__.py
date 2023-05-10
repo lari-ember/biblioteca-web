fb020decc8d04b911501ba3868d59bf342f8fb59
Larissa Ember <lari.ember@hotmail.com>
2023-05-10 15:21:56 -0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -12,6 +12,8 @@ from werkzeug.security import check_password_hash, generate_password_hash
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URL'] = 'slqlite:///storage.db'
+de = SQLAlchemy(app)
 
 from app.controllers import defaut
 
@@ -22,28 +24,6 @@ login_manager.login_view = 'login'
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
-
-class Book(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    code = db.Column(db.String(50), unique=True, nullable=False)
-    title = db.Column(db.String(200), nullable=False)
-    author = db.Column(db.String(100), nullable=False)
-    publisher = db.Column(db.String(100), nullable=False)
-    year = db.Column(db.Integer, nullable=False)
-    pages = db.Column(db.Integer, nullable=False)
-    status = db.Column(db.String(50), nullable=False)  # 'available', 'borrowed', or 'ex-libris'
-    format = db.Column(db.String(50), nullable=False)  # 'physical', 'e-book', or 'pdf'
-    genre = db.Column(db.String(50), nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(50), unique=True, nullable=False)
-    password = db.Column(db.String(100), nullable=False)
-    books = db.relationship('Book', backref='user', lazy=True)
-
-    def __repr__(self):
-        return f'<User {self.username}>'
 
 @app.cli.command('create_tables')
 #@login_required

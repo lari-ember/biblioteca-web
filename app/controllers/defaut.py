96d215c333fc05bb3a179105e0932af7c750b53d
Larissa Ember <lari.ember@hotmail.com>
2023-05-25 15:34:35 -0300

    add 001

--- app/controllers/defaut.py
+++ app/controllers/defaut.py
@@ -1,3 +1,4 @@
+import csv
 from flask import Flask, current_app, render_template, request, redirect, flash, session, url_for
 from flask_login import LoginManager, current_user, login_required, login_user, logout_user, UserMixin
 from datetime import date
@@ -6,6 +7,30 @@ from app.models.forms import LoginForm, RegistrationForm, BookForm
 from app.models.code_book import generate_book_code
 from flask_login import login_user, logout_user
 
+
+def add_books_from_csv(file_path):
+    user = get_logged_in_user()
+    with open(file_path, 'r') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            book = Book(
+                user_id=user.id,
+                code=row['codigo'],
+                title=row['titulo'],
+                author=row['autore'],
+                publisher=row['editora'],
+                year=row['ano'],
+                pages=row['paginas'],
+                read=row['lido'],
+                genre=row['genero'],
+                status=row['possui'],
+                format=row['formato']
+            )
+            db.session.add(book)
+            db.session.commit()
+
+# Caminho para o arquivo CSV de exemplo
+csv_file_path = 'C:/Users/bolsista.SFDA-DOACAO-BOL/Documents/GitHub/Biblioteca/acervo.csv'
 
 
 @lm.user_loader

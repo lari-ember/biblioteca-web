0d173de781d311e408b971151679a4407454cbc2
Larissa Ember <lari.ember@hotmail.com>
2023-10-23 14:43:25 -0300

    Update run.py

--- run.py
+++ run.py
@@ -2,4 +2,3 @@ from app import app
 
 if __name__ == '__main__':
     app.run(debug=True)
-    #https://www.airbnb.com.br/s/Curitiba-~-PR/homes?tab_id=home_tab&refinement_paths%5B%5D=%2Fhomes&flexible_trip_lengths%5B%5D=one_week&monthly_start_date=2023-09-01&monthly_length=3&price_filter_input_type=0&price_filter_num_nights=15&channel=EXPLORE&date_picker_type=calendar&checkin=2023-12-12&checkout=2023-12-27&query=Curitiba%20-%20PR&place_id=ChIJ3bPNUVPj3JQRCejLuqVrL20&source=structured_search_input_header&search_type=autocomplete_click&federated_search_session_id=13b0c328-7620-471c-b6ad-51904b94037f&pagination_search=true&cursor=eyJzZWN0aW9uX29mZnNldCI6MywiaXRlbXNfb2Zmc2V0IjoxOCwidmVyc2lvbiI6MX0%3D
